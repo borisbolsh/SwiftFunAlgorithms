fbd4e5ea5bb33208b0430437bb87c29549190a38
Boris Bolshakov <bolsh.bo@yandex.ru>
2021-09-19 20:39:14 +0300

    37 valid parantheses

---
+++ 37_ValidParentheses.playground/Contents.swift
@@ -0,0 +1,26 @@
+func validParentheses(_ s: String) -> Bool {
+    var stack = [Character]()
+    
+    for char in s {
+        if char == "(" || char == "[" || char == "{" {
+            stack.append(char)
+        } else if char == ")" {
+            guard stack.count != 0 && stack.removeLast() == "(" else {
+                return false
+            }
+        } else if char == "]" {
+            guard stack.count != 0 && stack.removeLast() == "[" else {
+                return false
+            }
+        } else if char == "}" {
+            guard stack.count != 0 && stack.removeLast() == "{" else {
+                return false
+            }
+        }
+    }
+    
+    return stack.isEmpty
+}
+
+validParentheses("()[]{}")  // true
+validParentheses("()]{}")   // false
